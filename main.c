d3722339bd5c5a907256ff166f8806a42c4c93cb
Robin Obůrka <robin.oburka@nic.cz>
2016-02-24 09:53:41 +0100

    Clean up forgotten includes

--- main.c
+++ main.c
@@ -20,13 +20,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <errno.h>
-#include <string.h>
 #include <linux/i2c-dev.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/mman.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <assert.h>
